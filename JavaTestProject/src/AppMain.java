2d802f94bc8ea513333ecd43de4400e739138036
Jan Kraus <jan.erik.kraus@t-online.de>
2022-05-17 15:32:05 +0200

    Teilaufgabe 2: Vergleiche 2 Zahlen

--- JavaTestProject/src/AppMain.java
+++ JavaTestProject/src/AppMain.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AppMain {
     public static void main(String[] args) {
 
-        System.out.println("Hello Test!");
+        System.out.println("Taschenrechner \nAufgabe 1:");
 
         int s1;
         int s2;
@@ -19,7 +19,6 @@ public class AppMain {
 
         System.out.println("Operator eingeben");
         operator = scanner.next();
-
         operator.replaceAll(" ", "");
 
         if (operator.equals("+")){
@@ -38,5 +37,23 @@ public class AppMain {
 
         System.out.println(s1 + operator + s2 + "=");
         System.out.println(resultText + "\n" + result);
+
+        System.out.println("Zahlen vergleichen \nAufgabe 2:");
+
+        System.out.println("Erste Zahl eingeben:");
+        s1 = scanner.nextInt();
+        System.out.println("Zweite Zahl eingeben:");
+        s2 = scanner.nextInt();
+
+        if(s1!=s2){
+            if(s1<s2){
+                System.out.println(s1 + " ist kleiner als " + s2);
+            } else {
+                System.out.println(s1 + " ist größer als " + s2);
+            }
+        } else {
+            System.out.println(s1 + " und " + s2 + " sind gleich.");
+        }
+
     }
 }
